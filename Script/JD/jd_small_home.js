47fc35bc32b969312514ce5fb310abdb47ea94ce
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-08 13:33:14 +0800

    Update jd_small_home.js

--- Script/JD/jd_small_home.js
+++ Script/JD/jd_small_home.js
@@ -19,7 +19,7 @@ https://h5.m.jd.com/babelDiy/Zeus/2HFSytEAN99VPmMGZ6V4EYWus1x/index.html
 ===============Quantumultx===============
 [task_local]
 #东东小窝
-16 22 * * * jd_small_home.js, tag=东东小窝, img-url=https://raw.githubusercontent.com/58xinian/icon/master/ddxw.png, enabled=true
+16 22 * * * https://raw.githubusercontent.com/zzm1908822163/QuantumultX/master/Script/JD/jd_small_home.js, tag=东东小窝, img-url=https://raw.githubusercontent.com/58xinian/icon/master/ddxw.png, enabled=true
 
 ================Loon==============
 [Script]
