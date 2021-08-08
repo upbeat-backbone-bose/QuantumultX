8e18250748fb3a10d99119f4259cc8657d1c7d6c
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-08 13:42:19 +0800

    Update jx_sign.js

--- Script/JD/jx_sign.js
+++ Script/JD/jx_sign.js
@@ -9,7 +9,7 @@ cron 20 1 * * * jx_sign.js
 ============Quantumultx===============
 [task_local]
 #京喜签到
-20 1 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jx_sign.js, tag=京喜签到, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
+20 1 * * * https://raw.githubusercontent.com/zzm1908822163/QuantumultX/master/Script/JD/jx_sign.js, tag=京喜签到, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
 
 ================Loon==============
 [Script]
