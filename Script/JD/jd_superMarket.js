b74fcb3658bc73bcf14c6b013ed24f86041e3ddd
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-08 13:39:18 +0800

    Update jd_superMarket.js

--- Script/JD/jd_superMarket.js
+++ Script/JD/jd_superMarket.js
@@ -8,7 +8,7 @@ Some Functions Modified From https://github.com/Zero-S1/JD_tools/blob/master/JD_
 =================QuantumultX==============
 [task_local]
 #东东超市
-11 * * * * jd_superMarket.js, tag=东东超市, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
+11 * * * * https://raw.githubusercontent.com/zzm1908822163/QuantumultX/master/Script/JD/jd_superMarket.js, tag=东东超市, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
 ===========Loon===============
 [Script]
 cron "11 * * * *" script-path=jd_superMarket.js,tag=东东超市
