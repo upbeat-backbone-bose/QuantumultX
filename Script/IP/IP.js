1accfd910c915046bbd78633c1c1be492fedd425
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-09 08:57:49 +0800

    Update IP.js

--- Script/IP/IP.js
+++ Script/IP/IP.js
@@ -4,9 +4,10 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var subtitle = ' ' + obj['isp'];
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
 
 
-$done({subtitle, ip, description});
+$done({title, subtitle, ip, description});
